03804e57a1ffaeb417c2f1ebd110750e0ef92cb6
adarzh-sys <adoo2001@gmail.com>
2022-07-27 08:00:39 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -3,28 +3,39 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Navbar from 'react-bootstrap/Navbar';
+import Stack from 'react-bootstrap/Stack';
+
 
 function App() {
+
   return (
+    
     <div className="App">
+
       <header className="App-header">
 
-        
+
+
       <Navbar bg="light" expand="lg" fixed="top">
       <Container fluid>
-
+        <Stack direction="horizontal" gap ={5}>
         <Navbar.Brand href="#home">
         <img
               alt=""
-              src='/img/clouds.svg'
+              src='/clouds.svg'
               width="30"
               height="30"
               className="d-inline-block align-top"
-            />{' '}
+            />{'    '}
             FAL Weather Station
         </Navbar.Brand>
+
+        </Stack>
+        
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
+        <Navbar.Collapse 
+          id="navbarScroll"
+          className="justify-content-end">
           <Form className="d-flex">
             <Form.Control
               type="search"
@@ -38,26 +49,15 @@ function App() {
       </Container>
       </Navbar>
        
-
+      
 
         <div class="right-container">
-
-        <card>
-        <iframe
-          title="moisture"
-          src="https://thingspeak.com/channels/1793216/charts/5?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
-          width = "425"
-          height = "225"
-          scrolling="no" 
-        >
-        </iframe>
-        </card>
-
+        
 
         <card>
         <iframe
           title="temp"
-          src="https://thingspeak.com/channels/1793216/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+          src="https://thingspeak.com/channels/1793216/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line"
           width = "425"
           height = "225"
           scrolling="no" 
@@ -68,7 +68,7 @@ function App() {
         <card>
         <iframe
           title="humidity"
-          src="https://thingspeak.com/channels/1793216/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+          src="https://thingspeak.com/channels/1793216/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line"
           width = "425"
           height = "225"
           scrolling="no" 
@@ -79,7 +79,7 @@ function App() {
         <card>
         <iframe
           title="moisture"
-          src="https://thingspeak.com/channels/1793216/charts/5?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+          src="https://thingspeak.com/channels/1793216/charts/5?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Soil+Moisture&type=line "
           width = "425"
           height = "225"
           scrolling="no" 
@@ -90,7 +90,7 @@ function App() {
         <card>
         <iframe
           title="pressure "
-          src="https://thingspeak.com/channels/1793216/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+          src="https://thingspeak.com/channels/1793216/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Pressure&type=line"
           width = "425"
           height = "225"
         >
@@ -99,8 +99,21 @@ function App() {
 
         <card>
         <iframe
+          title="pressure gauge"
+          src="https://thingspeak.com/channels/1793216/widgets/499596"
+          width = "425"
+          height = "225"
+          scrolling="no" 
+          border-radius= "10"
+        >
+        </iframe>
+        </card>
+
+
+        <card>
+        <iframe
           title="altitude"
-          src="https://thingspeak.com/channels/1793216/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+          src="https://thingspeak.com/channels/1793216/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Altitude&type=line"
           width = "425"
           height = "225"
           scrolling="no" 
@@ -109,7 +122,17 @@ function App() {
         </card>
 
         </div>
+
+
       </header>
+
+
+      {/* <footer class="bg-light text-center text-lg-start">
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2020 Copyright:
+      <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+      </div>
+      </footer> */}
     </div>
     
   );
